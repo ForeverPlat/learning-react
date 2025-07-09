323e1ac5da30bed35827442781de5f578d771c64
foreverplat <luqman.o.ajani@gmail.com>
2025-07-09 05:52:29 -0600

    use state basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
-import React from 'react'
-import Weather from './components/Weather';
-import UserStatus from './components/UserStatus';
-import Greeting from './components/Greeting';
+import React, { use } from 'react'
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
 
-  return <div>
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
 
+  // ----------
 
-    {/* <Weather temperature={16}/> */}
+  const [friends, setFriends] = useState(['Alex', 'John']);
 
-    {/* <UserStatus loggedIn={true} isAdmin={true} /> */}
+  const addOneFriend = () => setFriends([...friends, "Luqman"]);
+  const removeFriend = () => setFriends(friends.filter(f => f !== 'John'));
 
-    <Greeting timeOfDay="morning" />
-  </div>
+  const updateOneFriend = () => {
+    setFriends(friends.map(f => f === 'Alex' ? 'Alex Smith' : f))
+  }
+
+  // ---------
+
+  const [movie, setMovie] = useState({
+    title: 'Equalizer',
+    ratings: 7,
+  });
+
+  const changeRating = () => {
+    // const copyMovie = {
+    //   ...movie,
+    //   ratings: 5  
+    // }
+
+    // setMovie(copyMovie);
+    setMovie({...movie, ratings: 5});
+  }
+
   
+
+  return (
+    <section>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+
+      
+      {
+        friends.map(f => (
+          <li key={ Math.random() }>{ f }</li>
+        ))
+      }
+
+      <button onClick={addOneFriend}>Add New Friend</button>
+      <button onClick={removeFriend}>Remove One Friend</button>
+      <button onClick={updateOneFriend}>Update One Friend</button>
+
+
+      <h1>{ movie.title }</h1>
+      <p>Ratings: { movie.ratings }</p>
+      <button onClick={changeRating}>Change Rating</button>
+
+    </section>
+  );
 
 }
 
