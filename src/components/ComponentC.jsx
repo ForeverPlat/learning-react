928ee9625e84c124f0efbb1f47fb7067558c27e7
foreverplat <luqman.o.ajani@gmail.com>
2025-07-15 09:19:37 -0600

    useContext hook

--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
+import { useContext } from 'react'
 import { Data, Data1 } from '../App'
 
 
 const ComponentC = () => {
+    const userName = useContext(Data)
+    const age = useContext(Data1);
+
   return (
-    <Data.Consumer>
-        {
-            (name) => {
-                // return <h1>{name}</h1>
-                return (
-                    <Data1.Consumer>
-                        {
-                            (age) => {
-                                return <h1>My name is: {name} and I'm {age} years old.</h1>
-                            }
-                        }
-                    </Data1.Consumer>
-                )
-            }
-        }
-    </Data.Consumer>
+    <h1>My name is {userName} and I am {age} years old</h1>
   )
 }
 
