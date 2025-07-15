7dad04080fd5d2b0e10ae0da4fa9dfe3776ddf49
foreverplat <luqman.o.ajani@gmail.com>
2025-07-15 09:16:32 -0600

    context api basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import BasicEffect from './components/BasicEffect'
-import CounterEffect from './components/CounterEffect'
-import FetchDataEffect from './components/FetchDataEffect'
+import { createContext } from 'react'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
+
+export const Data = createContext();
+export const Data1 = createContext();
 
 const App = () => {
+
+  const name = 'Luqman';
+  const age = 18;
+
   return (
     <div>
-      {/* <BasicEffect /> */}
-      {/* <CounterEffect /> */}
-      <FetchDataEffect />
+
+      <Data.Provider value={name}>
+        <Data1.Provider value={age}>
+          <ComponentA />
+        </Data1.Provider>
+      </Data.Provider>  
+
     </div>
   )
 }
