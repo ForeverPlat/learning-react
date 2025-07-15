3596c5afb13f81c1b828870bfe30871278965964
foreverplat <luqman.o.ajani@gmail.com>
2025-07-15 10:12:22 -0600

    useReducer T.T, i'm soo cooked

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
-import UserProfile from './components/UserProfile';
-import UpdateUser from './components/UpdateUser';
-import { UserProvider } from './UserContext';
+import { useReducer } from 'react'
+
+const initialState = {count: 0};
+
+//  state: what we update,  action: how we are updating it
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {...state, count: state.count + 1};
+    case "decrement":
+      return {...state, count: state.count - 1};
+    case "reset":
+      return {...state, count: 0}
+    default:
+      return state;
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <UserProvider>
-      <UserProfile />
-      <UpdateUser />
-    </UserProvider>
-  )
+  return <div>
+    <h1>Count: { state.count }</h1>
+    <button onClick={() => dispatch({ "type": "increment" })}>+</button>
+    <button onClick={() => dispatch({ "type": "decrement" })}>-</button>
+    <button onClick={() => dispatch({ "type": "reset" })}>Reset</button>
+  </div>
 }
 
 export default App
