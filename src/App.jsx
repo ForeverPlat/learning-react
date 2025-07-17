906b29b63ab4b7004898f6de19187dc22f37fec6
foreverplat <luqman.o.ajani@gmail.com>
2025-07-17 04:53:18 -0600

    useReducer practice, this one is confusing go over it more

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import React from 'react'
-import { useReducer } from 'react'
-
-const initialState = {count: 0};
-
-//  state: what we update,  action: how we are updating it
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return {...state, count: state.count + 1};
-    case "decrement":
-      return {...state, count: state.count - 1};
-    case "reset":
-      return {...state, count: 0}
-    default:
-      return state;
-  }
-}
+import Counter from './components/Counter'
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  return <div>
-    <h1>Count: { state.count }</h1>
-    <button onClick={() => dispatch({ "type": "increment" })}>+</button>
-    <button onClick={() => dispatch({ "type": "decrement" })}>-</button>
-    <button onClick={() => dispatch({ "type": "reset" })}>Reset</button>
-  </div>
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
 }
 
 export default App
